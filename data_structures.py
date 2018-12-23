c5c002d0cf629249aecd9f39310e2d5c74a27d3a
malekchahin <hoodieal97@gmail.com>
2018-12-23 01:07:45 +0100

    - Tree class implemented, serves as a binary search tree

--- data_structures.py
+++ data_structures.py
@@ -1,4 +1,4 @@
-from typing import Generic, List, TypeVar
+from typing import Generic, List, TypeVar, Callable
 
 _T = TypeVar('_T')
 
@@ -16,6 +16,9 @@ class BaseNode(Generic[_T]):
 
     def __del__(self):
         del self
+
+    def __repr__(self):
+        return str(self.item)
 
 
 class ListNode(BaseNode):
@@ -35,8 +38,27 @@ class ListNode(BaseNode):
         del self.successor
         super().__del__()
 
+
+class TreeNode(BaseNode):
+
+    def __init__(self, item: _T):
+
+        super().__init__(item)
+        self.right = None
+        self.left = None
+
+    def __del__(self):
+
+        del self.right
+        del self.left
+        super().__del__()
+
     def __repr__(self):
-        return str(self.item)
+        return '{} -> {}, {}'.format(
+            self.item,
+            self.left.item if self.left is not None else None,
+            self.right.item if self.right is not None else None
+        )
 
 
 class Queue(Generic[_T]):
@@ -89,7 +111,7 @@ class Queue(Generic[_T]):
 
         return l.__repr__()
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.first is None
 
     def __del__(self):
@@ -98,3 +120,116 @@ class Queue(Generic[_T]):
             self.pop()
         del self
 
+
+class Tree(Generic[_T]):
+
+    def __init__(self, *iterable: _T, comparable: Callable[[_T, _T], int]):
+
+        self.root = None
+        self.comparable = comparable
+
+        for i in iterable:
+            self.add(i)
+
+    def _add(self, node: TreeNode, item: _T) -> None:
+
+        if self.comparable(item, node.item) < 0:
+            if node.left is None:
+                node.left = TreeNode(item)
+            else:
+                self._add(node.left, item)
+        else:
+            if node.right is None:
+                node.right = TreeNode(item)
+            else:
+                self._add(node.right, item)
+
+    def _remove(self, node: TreeNode, item: _T) -> None:
+
+        if node is None:
+            return None
+
+        predecessor = None
+
+        while True:
+
+            if self.comparable(item, node.item) < 0:
+                predecessor = node
+                node = node.left
+            elif self.comparable(item, node.item) > 0:
+                predecessor = node
+                node = node.right
+
+            else:
+                if node.right is not None:
+                    item = self._min(node.right)
+                    node.item = item
+                    predecessor = node
+                    node = node.right
+                elif node.left is not None:
+                    item = self._max(node.left)
+                    node.item = item
+                    predecessor = node
+                    node = node.left
+                else:
+                    if predecessor.left is node:
+                        del node
+                        predecessor.left = None
+                    else:
+                        del node
+                        predecessor.right = None
+                    break
+
+    def _pass_in_order(self, node: TreeNode, func: Callable[[_T], None]) -> None:
+
+        if node.left is not None:
+            self._pass_in_order(node.left, func)
+
+        func(node.item)
+
+        if node.right is not None:
+            self._pass_in_order(node.right, func)
+
+    def _max(self, node: TreeNode) -> _T:
+
+        if node is None:
+            return None
+
+        if node.right is None:
+            return node.item
+        else:
+            return self._max(node.right)
+
+    def _min(self, node: TreeNode) -> _T:
+
+        if node is None:
+            return None
+        if node.left is None:
+            return node.item
+        else:
+            return self._min(node.left)
+
+    def add(self, item: _T) -> None:
+
+        if self.root is None:
+            self.root = TreeNode(item)
+        else:
+            self._add(self.root, item)
+
+    def remove(self, item: _T) -> None:
+        return self._remove(self.root, item)
+
+    def pass_in_order(self, func: Callable[[_T], None]) -> None:
+
+        if self.root is not None:
+            self._pass_in_order(self.root, func)
+
+
+class AVLTree(Tree):
+    pass
+
+
+t = Tree(4, 2, 1, 6, -1, 6, comparable=lambda x, y: x-y)
+
+t.remove(4)
+print(t.root)
